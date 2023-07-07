ec350b1ad9f6cf15b1e376d4eab858b0e774494a
yoki <yoki@satus-MacBook-Air.local>
2023-07-07 18:14:26 +0700

    add conditional operator and or

--- src/queBuiMongo.ts
+++ src/queBuiMongo.ts
@@ -1,46 +1,61 @@
-export function queBuiMongo(param: {
-  schema: any, req: {
-      name: string,
-      relations?: [{
-          name: string,
-          alias?: string
-          relation?: [],
-      }],
-      filter?: {},
+interface mongoSchema {
+  name: string,
+  relations?: [mongoSchema],
+  filter?: {},
+}
+
+function convertLike(value: string) {
+  const regex: any = new RegExp(
+    '.*' + value + '.*',
+    'i',
+  );
+
+  return { "$regex": regex }
+}
+
+function convertAndOr(filterValue){
+  for (let orAnd of filterValue) {
+    for (const search in orAnd) {
+      if(search != '$'){
+        orAnd[search] = convertLike(orAnd[search])
+      }
+      else if(['$and', '$or'].indexOf(search) != -1) {
+        convertAndOr(filterValue)
+      }
+    }
   }
-}) {
-  let resp:any[] = [];
+}
+
+export function queBuiMongo(param: { schema: any, req: mongoSchema }) {
+  let resp: any[] = [];
   const { schema, req } = param;
 
   if (req?.relations) {
-      req.relations.forEach(rel => {
-        try {
-          resp = [...resp, ...(schema[req.name].relations[rel.name])];
-        } catch (error) {
-          
-        }
-      });
+    req.relations.forEach(rel => {
+      try {
+        resp = [...resp, ...(schema[req.name].relations[rel.name])];
+      } catch (error) {
+
+      }
+    });
   }
 
-  if(req?.filter) {
-
-    Object.keys(req.filter).forEach(k=>{
-      if(k[0] != '$') {
-        const regex: any = new RegExp(
-          '.*' + req.filter[k] + '.*',
-          'i',
-        );
-        req.filter[k] = {
-          "$regex": regex
-        }
+  if (req?.filter) {
+    Object.keys(req.filter).forEach(k => {
+      let filterValue = req.filter[k]
+      if (k[0] != '$') {
+        req.filter[k] = convertLike(filterValue)
+      }
+      else if(['$and', '$or'].indexOf(k) != -1) {
+        convertAndOr(filterValue)
       }
     });
 
     resp = [
-      ...resp, 
+      ...resp,
       {
         $match: {
-          $and: [ 
+          $and: [
             req.filter
           ],
         }
--- src/sample/index.ts
+++ src/sample/index.ts
@@ -1,15 +1,23 @@
 import { queBuiMongo } from "../queBuiMongo";
 import scheme from "./scheme";
 
-console.log('test', JSON.stringify(
+console.log('test',
   queBuiMongo({
     schema: scheme,
     req: {
-      name: 'users',
-      relations: [{name: 'client'}],
-      filter: {
-        "client.name": "o"
+      "name": "users",
+      "relations": [
+        {
+          "name": "client"
+        }
+      ],
+      "filter": {
+        "$or": [
+          {
+            "client.name": "o"
+          }
+        ]
       }
     }
-  }))
+  })[2]['$match']['$and'][0]
 );
